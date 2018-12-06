251a1a939c75add5cc74fcc0cd4d073581883c7e
LRVerkin <louise.verkin@gmail.com>
2018-12-06 14:24:17 +0100

    added main file

---
+++ main_algen.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+class Individu:
+	'''
+	un individu est un MDP potentiel
+	de 12 caractères dans 0-9A-Z_
+
+	'''
+	def __init__(self):
+
+
+
+
+	def GenoToPheno(self):
+
+
+
+		return
+
+
+	def PhenoToGeno(self):
+
+		
+
+
+
+
+
